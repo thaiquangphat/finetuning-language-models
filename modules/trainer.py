ab543af299aad5349ef63daab30f0bbc1a30766e
thaiquangphat <quangphatthai@gmail.com>
2025-04-12 08:17:41 +0700

    Add finetuning pipeline

--- modules/trainer.py
+++ modules/trainer.py
@@ -10,8 +10,8 @@ import os
 from huggingface_hub import login
 
 # For preprocessing
-from data_loader import load_squad, load_wmt, load_imdb, SquadDataset, WMTDataset, IMDBDataset
-from model_loader import load_t5_base, load_bart_base, load_prophetnet_large
+from modules.datasets import load_squad, load_wmt, load_imdb, SquadDataset, WMTDataset, IMDBDataset
+from modules.models import load_t5_base, load_bart_base, load_prophetnet_large
 
 # ===============================================================================================
 def get_model_tokenizer(model, device):
@@ -29,7 +29,7 @@ def get_model_tokenizer(model, device):
 def get_dataset(dataset, test):
     loaders = {
         'squad': (load_squad, SquadDataset),
-        'wmt': (load_wmt, WMTDataset),
+        'wmt16_en_de': (load_wmt, WMTDataset),
         'imdb': (load_imdb, IMDBDataset)
     }
 
@@ -43,9 +43,10 @@ def get_dataset(dataset, test):
 # ===============================================================================================
 
 class BaseTrainer:
-    def __init__(self, device, model, dataset, test=False):
+    def __init__(self, device, model, dataset, finetune, test=False):
         self.device = device
-        self.dataset = dataset
+        self.dataset_name = dataset
+        self.finetune = finetune
         self.model, self.tokenizer = get_model_tokenizer(model, self.device)
         self.train_data, self.test_data, self.val_data = get_dataset(dataset, test)
 
@@ -68,7 +69,7 @@ class BaseTrainer:
             weight_decay=weight_decay,
             logging_dir=os.path.join(os.getenv("WANDB_NAME"), "logs"),
             logging_steps=logging_steps,
-            eval_strategy="epoch",
+            evaluation_strategy="epoch",
             save_strategy="epoch",
             learning_rate=learning_rate,
             load_best_model_at_end=True,
@@ -76,47 +77,52 @@ class BaseTrainer:
             run_name=os.getenv("WANDB_NAME")
         )
 
+    def apply_finetune_strategy(self):
+        if self.finetune_type == "lora":
+            lora_config = LoraConfig(
+                task_type=TaskType.SEQ_2_SEQ_LM,
+                r=8,
+                lora_alpha=32,
+                lora_dropout=0.1,
+                target_modules=["q_proj", "v_proj"],
+                bias="none"
+            )
+            self.model = get_peft_model(self.model, lora_config)
+
+        elif self.finetune_type == "adapters":
+            init(self.model)
+            adapter_config = AdapterConfig.load("pfeiffer", reduction_factor=16, non_linearity="relu")
+            self.model.add_adapter(self.dataset_name, config=adapter_config)
+            self.model.train_adapter(self.dataset_name)
+            self.model.set_active_adapters(self.dataset_name)
+
+        # "full" fine-tuning does not require modification
+
     def train_and_save(self, trainer, saved_model):
         trainer.train()
         trainer.save_model(saved_model)
         self.tokenizer.save_pretrained(saved_model)
 
-class FullFineTuneTrainer(BaseTrainer):
-    def run(self, saved_model, num_train_epochs=10, learning_rate=5e-5, weight_decay=0.02, logging_steps=1):
-        self.login_wandb('phat-ft-nlp', saved_model)
-        args = self.get_training_args(num_train_epochs, learning_rate, weight_decay, logging_steps)
-
-        trainer = Trainer(model=self.model, args=args, train_dataset=self.train_data, eval_dataset=self.val_data)
-        self.train_and_save(trainer, saved_model)
-
-class LoRaTrainer(BaseTrainer):
-    def run(self, saved_model, num_train_epochs=10, learning_rate=3e-4, weight_decay=0.02, logging_steps=1):
+    def run(self, saved_model, num_train_epochs=10, learning_rate=None, weight_decay=0.02, logging_steps=1):
         self.login_wandb('phat-ft-nlp', saved_model)
 
-        lora_config = LoraConfig(
-            task_type=TaskType.SEQ_2_SEQ_LM,
-            r=8,
-            lora_alpha=32,
-            lora_dropout=0.1,
-            target_modules=["q_proj", "v_proj"],
-            bias="none"
-        )
-        self.model = get_peft_model(self.model, lora_config)
+        # Set default learning rate per fine-tune type
+        if learning_rate is None:
+            if self.finetune_type == "full":
+                learning_rate = 5e-5
+            elif self.finetune_type == "lora":
+                learning_rate = 3e-4
+            elif self.finetune_type == "adapters":
+                learning_rate = 1e-6
 
+        # Applying finetune strategy
+        self.apply_finetune_strategy()
+
+        # Setting up training arguments
         args = self.get_training_args(num_train_epochs, learning_rate, weight_decay, logging_steps)
+
+        # Start training loop
         trainer = Trainer(model=self.model, args=args, train_dataset=self.train_data, eval_dataset=self.val_data)
-        self.train_and_save(trainer, saved_model)
 
-class AdaptersTrainer(BaseTrainer):
-    def run(self, saved_model, num_train_epochs=10, learning_rate=1e-6, weight_decay=0.02, logging_steps=1):
-        self.login_wandb('phat-ft-nlp', saved_model)
-
-        init(self.model)
-        adapter_config = AdapterConfig.load("pfeiffer", reduction_factor=16, non_linearity="relu")
-        self.model.add_adapter(self.dataset, config=adapter_config)
-        self.model.train_adapter(self.dataset)
-        self.model.set_active_adapters(self.dataset)
-
-        args = self.get_training_args(num_train_epochs, learning_rate, weight_decay, logging_steps)
-        trainer = Trainer(model=self.model, args=args, train_dataset=self.train_data, eval_dataset=self.val_data)
+        # Save finetuned model
         self.train_and_save(trainer, saved_model)
