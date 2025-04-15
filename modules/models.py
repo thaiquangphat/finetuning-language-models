9ff8455182205c0e12a4e6de02aca976a21984f8
thaiquangphat <quangphatthai@gmail.com>
2025-04-15 23:17:40 +0700

    Fix Adapters for Bart, T5. TODO: ProphetNet

--- modules/models.py
+++ modules/models.py
@@ -1,37 +1,62 @@
 import torch
+import torch.nn as nn
 from transformers import (
     AutoModelForSeq2SeqLM, AutoTokenizer, T5Config, # For T5
     BartTokenizer, BartForConditionalGeneration, BartConfig, # For BART
     ProphetNetTokenizer, ProphetNetForConditionalGeneration, ProphetNetConfig # For ProphetNet
 )
-from adapters import AutoAdapterModel
+from peft import LoraConfig, get_peft_model, TaskType, PeftConfig
+from adapters import AutoAdapterModel, BartAdapterModel, AdapterConfig
+from peft import get_peft_model, TaskType
 
 # ============================= LOADING MODEL ============================= #
 
-def load_t5_base(name='t5-base', finetune_type='full', device='cpu'):
+def load_t5_base(name='t5-base', finetune_type='full', task='qa', device='cpu'):
     """
     Load the T5-Base model.
 
     Args:
+        name (str): name of the pretrained model.
+        finetune_type (str): name of the finetune technique.
+        task (str): name of the finetune task.
         device (torch): device used for training: cuda or cpu.
     Returns: 
         model (AutoModelForSeq2SeqLM): t5-base model mapped to device.
         tokenizer (AutoTokenizer): t5-base tokenizer mapped to device.
     """
 
-    if finetune_type != 'adapters':
+    # Load the model
+    if finetune_type == 'full':
         model = AutoModelForSeq2SeqLM.from_pretrained(name)
-    else:
+
+    elif finetune_type == 'lora':
+        model = AutoModelForSeq2SeqLM.from_pretrained(name)
+        lora_config = LoraConfig(
+            task_type=TaskType.SEQ_2_SEQ_LM,
+            r=8,
+            lora_alpha=32,
+            lora_dropout=0.1,
+            target_modules=['q', 'v'],
+            bias="none"
+        )
+        model = get_peft_model(model, lora_config)
+
+    else: # adapters
         config = T5Config.from_pretrained(name)
         model = AutoAdapterModel.from_pretrained(name, config=config)
-    
-    tokenizer = AutoTokenizer.from_pretrained(name)
 
+        model.add_adapter(task)
+        model.set_active_adapters(task)
+    
+    # Move model to device
     model.to(device)
+
+    # Load the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(name)
 
     return model, tokenizer
 
-def load_bart_base(name='bart-base', finetune_type='full', device='cpu'):
+def load_bart_base(name='bart-base', finetune_type='full', task='qa', device='cpu'):
     """
     Load the Bart-Base model.
 
@@ -42,19 +67,89 @@ def load_bart_base(name='bart-base', finetune_type='full', device='cpu'):
         tokenizer (BartTokenizer): bart-base tokenizer mapped to device.
     """
 
-    if finetune_type != 'adapters':
+    # Define adapter creation function
+    def make_adapter(in_dim, bottleneck_dim, out_dim):
+        adapter_layers = nn.Sequential(
+            nn.Linear(in_dim, bottleneck_dim),
+            nn.GELU(),
+            nn.Linear(bottleneck_dim, out_dim),
+        )
+        return adapter_layers
+
+    if finetune_type == 'full':
         model = BartForConditionalGeneration.from_pretrained(f'facebook/{name}')
+    
+    elif finetune_type == 'lora':
+        model = AutoModelForSeq2SeqLM.from_pretrained(f'facebook/{name}')
+        lora_config = LoraConfig(
+            task_type=TaskType.SEQ_2_SEQ_LM,
+            r=8,
+            lora_alpha=32,
+            lora_dropout=0.1,
+            target_modules=["q_proj", "v_proj"],
+            bias="none"
+        )
+        model = get_peft_model(model, lora_config)
+    
     else:
-        config = BartConfig.from_pretrained(f'facebook/{name}')
-        model = AutoAdapterModel.from_pretrained(f'facebook/{name}', config=config)
+        # 1. Load BART-base model
+        model = BartForConditionalGeneration.from_pretrained(f'facebook/{name}')
+        
+        # 2. Add adapters manually
+        bottleneck_size = 32  # Hyperparameter
+        hidden_size = model.config.d_model  # 768 for BART-base
 
-    tokenizer = BartTokenizer.from_pretrained(f'facebook/{name}')
+        # 3. Add adapters to encoder layers
+        for block_idx in range(len(model.model.encoder.layers)):
+            # Encoder: Insert 1st adapter after self-attention output
+            orig_layer_1 = model.model.encoder.layers[block_idx].self_attn.out_proj
+            adapter_layers_1 = make_adapter(
+                in_dim=hidden_size,
+                bottleneck_dim=bottleneck_size,
+                out_dim=hidden_size
+            )
+            # Create a new Sequential, but keep the reference to the original layer
+            model.model.encoder.layers[block_idx].self_attn.out_proj = nn.Sequential(orig_layer_1, adapter_layers_1)
 
+            # Encoder: Insert 2nd adapter after feed-forward output
+            orig_layer_2 = model.model.encoder.layers[block_idx].fc2
+            adapter_layers_2 = make_adapter(
+                in_dim=hidden_size,
+                bottleneck_dim=bottleneck_size,
+                out_dim=hidden_size
+            )
+            model.model.encoder.layers[block_idx].fc2 = nn.Sequential(orig_layer_2, adapter_layers_2)
+
+        # 4. Add adapters to decoder layers
+        for block_idx in range(len(model.model.decoder.layers)):
+            # Decoder: Insert 1st adapter after self-attention output
+            orig_layer_1 = model.model.decoder.layers[block_idx].self_attn.out_proj
+            adapter_layers_1 = make_adapter(
+                in_dim=hidden_size,
+                bottleneck_dim=bottleneck_size,
+                out_dim=hidden_size
+            )
+            model.model.decoder.layers[block_idx].self_attn.out_proj = nn.Sequential(orig_layer_1, adapter_layers_1)
+
+            # Decoder: Insert 2nd adapter after feed-forward output
+            orig_layer_2 = model.model.decoder.layers[block_idx].fc2
+            adapter_layers_2 = make_adapter(
+                in_dim=hidden_size,
+                bottleneck_dim=bottleneck_size,
+                out_dim=hidden_size
+            )
+            model.model.decoder.layers[block_idx].fc2 = nn.Sequential(orig_layer_2, adapter_layers_2)
+    
+    # Move model to device
     model.to(device)
+
+    # Load the tokenizer
+    tokenizer = BartTokenizer.from_pretrained(f'facebook/{name}')
 
     return model, tokenizer
 
-def load_prophetnet_large(name='prophetnet-large-uncased', finetune_type='full', device='cpu'):
+# TODO: ProphetNet Lora and Adapters
+def load_prophetnet_large(name='prophetnet-large-uncased', finetune_type='full', task='qa', device='cpu'):
     """
     Load the ProphetNet-Large model.
 
@@ -64,14 +159,26 @@ def load_prophetnet_large(name='prophetnet-large-uncased', finetune_type='full',
         model (ProphetNetForConditionalGeneration): prophetnet-base model mapped to device.
         tokenizer (ProphetNetTokenizer): prophetnet-base tokenizer mapped to device.
     """
-    if finetune_type != 'adapters':
+
+    if finetune_type == 'full':
         model = ProphetNetForConditionalGeneration.from_pretrained(f'microsoft/{name}')
+    
+    elif finetune_type == 'lora':
+        model = ProphetNetForConditionalGeneration.from_pretrained(f'microsoft/{name}')
+        lora_config = LoraConfig(
+            task_type=TaskType.SEQ_2_SEQ_LM,
+            r=8,
+            lora_alpha=32,
+            lora_dropout=0.1,
+            target_modules=["query_proj", "value_proj"],
+            bias="none"
+        )
+        model = get_peft_model(model, lora_config)
+    
     else:
         config = ProphetNetConfig.from_pretrained(f'microsoft/{name}')
         model = AutoAdapterModel.from_pretrained(f'microsoft/{name}', config=config)
 
     tokenizer = ProphetNetTokenizer.from_pretrained(f'microsoft/{name}')
-
-    model.to(device)
 
     return model, tokenizer
--- modules/trainer.py
+++ modules/trainer.py
@@ -1,20 +1,26 @@
 # For training
 from transformers import (
-    TrainingArguments, Trainer # For training
+    TrainingArguments, Trainer, # For training
+    DataCollatorForSeq2Seq
 )
-from peft import LoraConfig, get_peft_model, TaskType
-from adapters import AdapterConfig, AdapterTrainer, init
 
 # For login wandb
 import os
 from huggingface_hub import login
 
 # For preprocessing
-from modules.datasets import load_squad, load_wmt, load_imdb, SquadDataset, WMTDataset, IMDBDataset
+from modules.hfdata import load_squad, load_wmt, load_imdb, SquadDataset, WMTDataset, IMDBDataset
 from modules.models import load_t5_base, load_bart_base, load_prophetnet_large
 
+# Task mapper
+data2task = {
+    "squad": "question_answering",
+    "imdb": "text_sentiment_analysis",
+    "wmt16_en_de": "english_to_german_translation"
+}
+
 # ===============================================================================================
-def get_model_tokenizer(model, finetune_type, device):
+def get_model_tokenizer(model, finetune_type, task, device):
     loaders = {
         't5-base': load_t5_base,
         'bart-base': load_bart_base,
@@ -25,7 +31,7 @@ def get_model_tokenizer(model, finetune_type, device):
 
     for key in loaders:
         if key in model:
-            return loaders[key](model, finetune_type, device)
+            return loaders[key](model, finetune_type, task, device)
 
     raise NotImplementedError(
         f"Model '{model}' not implemented."
@@ -59,7 +65,7 @@ class BaseTrainer:
         self.finetune_type = finetune
         self.train_batch_size = train_batch_size
         self.eval_batch_size = eval_batch_size
-        self.model, self.tokenizer = get_model_tokenizer(model, finetune, self.device)
+        self.model, self.tokenizer = get_model_tokenizer(model, finetune, data2task[dataset], self.device)
         self.train_data, self.test_data, self.val_data = get_dataset(dataset, self.tokenizer, test)
 
     def set_wandb_api(self, wandb_token, wandb_api, project):
@@ -99,32 +105,6 @@ class BaseTrainer:
             use_cpu=use_prophet if use_prophet is True else use_cpu
         )
 
-    def apply_finetune_strategy(self):
-        if 't5-base' in self.model_name:
-            target_mods = ['q', 'v']
-        elif 'bart-base' in self.model_name:
-            target_mods = ["q_proj", "v_proj"]
-        elif 'prohpetnet-large-uncased' in self.model_name:
-            target_mods = ["query_proj", "value_proj"]
-            
-        if self.finetune_type == "lora":
-            lora_config = LoraConfig(
-                task_type=TaskType.SEQ_2_SEQ_LM,
-                r=8,
-                lora_alpha=32,
-                lora_dropout=0.1,
-                target_modules=target_mods,
-                bias="none"
-            )
-            self.model = get_peft_model(self.model, lora_config)
-
-        elif self.finetune_type == "adapters":
-            adapter_config = AdapterConfig.load("pfeiffer", reduction_factor=16, non_linearity="relu")
-            self.model.add_adapter(self.dataset_name, config=adapter_config)
-            self.model.train_adapter(self.dataset_name)
-
-        # "full" fine-tuning does not require modification
-
     def train_and_save(self, trainer, saved_model):
         print(
     f"""================================ Start finetuning ==================================
@@ -159,19 +139,25 @@ class BaseTrainer:
             elif self.finetune_type == "adapters":
                 learning_rate = 1e-6
 
-        # Applying finetune strategy
-        self.apply_finetune_strategy()
-
         # Setting up training arguments
         args = self.get_training_args(num_train_epochs, learning_rate, weight_decay, logging_steps, use_cpu)
 
-        # Start training loop
-        if self.finetune_type != 'adapters':
-            trainer_class = Trainer
-        else:
-            trainer_class = AdapterTrainer
+        # Seq2Seq collator
+        data_collator = DataCollatorForSeq2Seq(
+            tokenizer=self.tokenizer,
+            model=self.model,
+            padding=True,  # or "longest"
+            return_tensors="pt"
+        )
 
-        trainer = trainer_class(model=self.model, args=args, train_dataset=self.train_data, eval_dataset=self.val_data)
+        # Start training loop
+        trainer = Trainer(
+            model=self.model, 
+            args=args, 
+            train_dataset=self.train_data, 
+            eval_dataset=self.val_data, 
+            data_collator=data_collator
+        )
 
         # Save finetuned model
         self.train_and_save(trainer, saved_model)
