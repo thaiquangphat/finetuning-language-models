0ff4c6ce67c8d21d418ec86e2836449cdbe2ac48
thaiquangphat <quangphatthai@gmail.com>
2025-04-29 16:49:31 +0700

    adjust prompt

--- modules/data/preprocessing.py
+++ modules/data/preprocessing.py
@@ -166,7 +166,7 @@ def preprocess_imdb(dataset, tokenizer, max_input_length=512, max_target_length=
         return text
     
     # Extract review texts and labels
-    inputs = ["determine the sentiment: " + clean_imdb(text) for text in dataset["text"]]
+    inputs = ["determine text sentiment: " + clean_imdb(text) for text in dataset["text"]]
     # Convert numeric labels (0, 1) to text ("negative", "positive")
     targets = ["sentiment: " + ("positive" if label == 1 else "negative") for label in dataset["label"]]
     
